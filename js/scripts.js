865317cc5cd8b2171eaf6018121f9c7ac90c5509
Cameron Green <cgree78@gmail.com>
2018-01-03 19:49:52 -0600

    added the konami code to site

--- js/scripts.js
+++ js/scripts.js
@@ -10,20 +10,23 @@
     portfolio website.
 */
 
-(function($) {
+(function ($) {
 
     // Remove no-js class
     $('html').removeClass('no-js');
 
     // Animate to section when nav is clicked
-    $('header a').click(function(e) {
+    $('header a').click(function (e) {
 
         // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
+        if ($(this).hasClass('no-scroll'))
+            return;
 
         e.preventDefault();
         var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
+        var scrollDistance = $(heading)
+            .offset()
+            .top;
 
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
@@ -36,62 +39,96 @@
     });
 
     // Scroll to top
-    $('#to-top').click(function() {
+    $('#to-top').click(function () {
         $('html, body').animate({
             scrollTop: 0
         }, 500);
     });
 
     // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
+    $('#lead-down span').click(function () {
+        var scrollDistance = $('#lead')
+            .next()
+            .offset()
+            .top;
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
         }, 500);
     });
 
     // Create timeline
-    $('#experience-timeline').each(function() {
+    $('#experience-timeline').each(function () {
 
         $this = $(this); // Store reference to this
         $userContent = $this.children('div'); // user content
 
         // Create each timeline block
-        $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+        $userContent.each(function () {
+            $(this)
+                .addClass('vtimeline-content')
+                .wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
         });
 
         // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
-        });
+        $this
+            .find('.vtimeline-point')
+            .each(function () {
+                $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+            });
 
         // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
-            }
-        });
+        $this
+            .find('.vtimeline-content')
+            .each(function () {
+                var date = $(this).data('date');
+                if (date) {
+                    // Prepend if exists
+                    $(this)
+                        .parent()
+                        .prepend('<span class="vtimeline-date">' + date + '</span>');
+                }
+            });
 
     });
 
     // Open mobile menu
-    $('#mobile-menu-open').click(function() {
+    $('#mobile-menu-open').click(function () {
         $('header, body').addClass('active');
     });
 
     // Close mobile menu
-    $('#mobile-menu-close').click(function() {
+    $('#mobile-menu-close').click(function () {
         $('header, body').removeClass('active');
     });
 
     // Load additional projects
-    $('#view-more-projects').click(function(e){
+    $('#view-more-projects').click(function (e) {
         e.preventDefault();
-        $(this).fadeOut(300, function() {
+        $(this).fadeOut(300, function () {
             $('#more-projects').fadeIn(300);
         });
     });
 
 })(jQuery);
+
+/**
+ * Add Konami Code.
+ */
+var snowflakes = document.querySelector('#snowflakeDiv')
+var pressed = []
+var secretCode = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba'
+
+console.log("Psstt... try the konami code!")
+
+window.addEventListener('keyup', function (e) {
+    if (pressed.length > 100) {
+        pressed.length = 0
+    }
+    pressed.push(e.key)
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length)
+    if (pressed.join('').includes(secretCode)) {
+        snowflakes
+            .classList
+            .remove('hidden')
+    }
+});
